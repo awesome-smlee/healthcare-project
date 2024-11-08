2491179490c3056cd7e855a2cb0e8995bbdf5e2d
smlee <smlee94.dev@gmail.com>
2024-11-08 16:07:54 +0900

    taglib 삭제함, 다시 세팅해야함

--- src/main/java/com/insilicogen/healthcareproject/layer/presentation/controller/DataController.java
+++ src/main/java/com/insilicogen/healthcareproject/layer/presentation/controller/DataController.java
@@ -1,6 +1,7 @@
 package com.insilicogen.healthcareproject.layer.presentation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DataController {
     @GetMapping("/test")
     public String test() {
-        return "hello";
+        return "home";
     }
+
 }
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,11 @@
+<%--<%@ include file="/WEB-INF/include/taglib.jsp" %>--%>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>제목</title>
+</head>
+<body>
+    <h1>Welcome to the Home Page!</h1>
+    <p>This is the home page content.</p>
+</body>
+</html>
